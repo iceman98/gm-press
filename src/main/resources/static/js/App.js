79cf9edf1533ae0d5a9d94708f8fe73c97ca2302
David <iceman98@gmail.com>
2018-03-14 20:05:33 +0100

    serverless mode

--- src/main/resources/static/js/App.js
+++ src/main/resources/static/js/App.js
@@ -1,35 +1,101 @@
+function sign(number){
+    if(number>0){
+        return "+" + number;
+    } else {
+        return number;
+    }
+}
+
 function Statistic(props){
     var score = Math.floor((props.value - 10) / 2);
 
     return (
-        <div className="col card">
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                    <div className="text-right small">{props.name}</div>
-                    <div>{props.value}</div>
-                </li>
-                <li className="list-group-item">
-                    {score>0?'+':''}{score}
-                </li>
-            </ul>
+        <Box title={props.name}>
+            <div style={{borderBottom:"1px solid black", textAlign:"center"}}>
+                {props.value}
+            </div>
+            <div style={{textAlign:"center"}}>
+                {sign(score)}
+            </div>
+        </Box>
+    );
+}
+
+function Box(props){
+    return (
+        <div style={{border: "1px solid black", display: "table-cell"}}>
+            <div style={{textAlign: "right", fontSize: "xx-small", marginBottom: "-10px", color: "darkgray"}}>{props.title}</div>
+            <div>{props.children}</div>
         </div>
     );
+}
+
+function Skills(monster){
+    monster = monster.monster;
+    var skills = [];
+
+    if(monster.acrobatics) skills.push(<span>acrobatics {sign(monster.acrobatics)}</span>);
+    if(monster.arcana) skills.push(<span>arcana {sign(monster.arcana)}</span>);
+    if(monster.athletics) skills.push(<span>athletics {sign(monster.athletics)}</span>);
+    if(monster.deception) skills.push(<span>deception {sign(monster.deception)}</span>);
+    if(monster.history) skills.push(<span>history {sign(monster.history)}</span>);
+    if(monster.insight) skills.push(<span>insight {sign(monster.insight)}</span>);
+    if(monster.intimidation) skills.push(<span>intimidation {sign(monster.intimidation)}</span>);
+    if(monster.investigation) skills.push(<span>investigation {sign(monster.investigation)}</span>);
+    if(monster.medicine) skills.push(<span>medicine {sign(monster.medicine)}</span>);
+    if(monster.nature) skills.push(<span>nature {sign(monster.nature)}</span>);
+    if(monster.perception) skills.push(<span>perception {sign(monster.perception)}</span>);
+    if(monster.performance) skills.push(<span>performance {sign(monster.performance)}</span>);
+    if(monster.persuasion) skills.push(<span>persuasion {sign(monster.persuasion)}</span>);
+    if(monster.religion) skills.push(<span>religion {sign(monster.religion)}</span>);
+    if(monster.stealth) skills.push(<span>stealth {sign(monster.stealth)}</span>);
+    if(monster.survival) skills.push(<span>survival {sign(monster.survival)}</span>);
+
+    if(skills.length>0){
+        return (
+            <div>
+                <b>Skills</b>
+                {skills}
+            </div>
+        );
+    }
+
+    return <div>&nbsp;</div>;
+}
+
+function AbilitySaves(monster){
+    monster = monster.monster;
+    var saves = [];
+
+    if(monster.strength_save) saves.push(<span>Strength {sign(monster.strength_save)}</span>);
+    if(monster.dexterity_save) saves.push(<span>Dexterity {sign(monster.dexterity_save)}</span>);
+    if(monster.constitution_save) saves.push(<span>Constitution {sign(monster.constitution_save)}</span>);
+    if(monster.intelligence_save) saves.push(<span>Intelligence {sign(monster.intelligence_save)}</span>);
+    if(monster.wisdom_save) saves.push(<span>Wisdom {sign(monster.wisdom_save)}</span>);
+    if(monster.charisma_save) saves.push(<span>Charisma {sign(monster.charisma_save)}</span>);
+
+    if(saves.length>0){
+        return (
+            <div>
+                <b>Saving Throws</b>
+                {saves}
+            </div>
+        );
+    }
+
 }
 
 class App extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {filter: 'mino', monsters:[], selected:[]};
+        this.state = {filter: '', monsters:[], selected:[]};
     }
 
     componentDidMount(){
         var me = this;
-        this.updateSearch(me.state.filter, me);
-    }
 
-    updateSearch(query, me){
-        $.ajax('/api/monster/find?n=' + query, {dataType:'json', success: function(data){
+        $.ajax('data/monsters.js', {dataType:'json', success: function(data){
             me.setState({monsters:data});
 
             if(me.state.selected.length == 0){
@@ -39,6 +105,12 @@ class App extends React.Component {
     }
 
     render() {
+        var monsters = [];
+
+        this.state.monsters.map((m)=>{
+            if(this.state.filter == '' || m.name.toUpperCase().includes(this.state.filter.toUpperCase())) monsters.push(m);
+        });
+
         return (
             <div className="col">
                 <div className="row no-print">
@@ -49,43 +121,40 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     {this.state.selected.map((m)=>
-                        <div key={m.index} className="col-4">
-                            <div className="row col">
-                                <img className="card-img-top"/>
+                        <div key={m.index} style={{padding:"5px"}}>
+                            <div style={{borderBottom: "5px double black"}}>
+                                <img/>&nbsp;
                             </div>
-                            <div className="row col card">
-                                <div className="text-right small">Name</div>
-                                <div>{m.name}</div>
+                            <div>
+                                <Box title="Name">
+                                    {m.name}
+                                </Box>
                             </div>
-                            <div className="row">
-                                <div className="col card">
-                                    <div className="text-right small">Size</div>
-                                    <div>{m.size}</div>
-                                </div>
-                                <div className="col card">
-                                    <div className="text-right small">Type</div>
-                                    <div>{m.type}</div>
-                                </div>
-                                <div className="col card">
-                                    <div className="text-right small">Alignment</div>
-                                    <div>{m.alignment}</div>
-                                </div>
+                            <div style={{display:"table-row"}}>
+                                <Box title="Size">
+                                    {m.size}
+                                </Box>
+                                <Box title="Type">
+                                    {m.type}
+                                </Box>
+                                <Box title="Alignment">
+                                    {m.alignment}
+                                </Box>
                             </div>
-                            <div className="row">
-                                <div className="col card">
-                                    <div className="text-right small">AC</div>
-                                    <div>{m.armor_class}</div>
-                                </div>
-                                <div className="col card">
-                                    <div className="text-right small">HP</div>
-                                    <div>{m.hit_points} ({m.hit_dice})</div>
-                                </div>
+                            <div>
+                                <Box title="AC">
+                                    {m.armor_class}
+                                </Box>
+                                <Box title="HP">
+                                    {m.hit_points} ({m.hit_dice})
+                                </Box>
                             </div>
-                            <div className="row col card">
-                                <div className="text-right small">Speed</div>
-                                <div>{m.speed}</div>
+                            <div>
+                                <Box title="Speed">
+                                    {m.speed}
+                                </Box>
                             </div>
-                            <div className="row">
+                            <div>
                                 <Statistic name="STR" value={m.strength}/>
                                 <Statistic name="DEX" value={m.dexterity}/>
                                 <Statistic name="CON" value={m.constitution}/>
@@ -93,12 +162,44 @@ class App extends React.Component {
                                 <Statistic name="WIS" value={m.wisdom}/>
                                 <Statistic name="CHA" value={m.charisma}/>
                             </div>
+                            <div>
+                                <Box title="Senses">
+                                    {m.senses}
+                                </Box>
+                            </div>
+                            <div>
+                                <div>
+                                    <Box title="Passive Perception">
+                                        {10 + (m.perception? m.perception:Math.floor((m.wisdom - 10) / 2))}
+                                    </Box>
+                                    <Box title="Challenge">
+                                        {(m.challenge_rating == 0 || m.challenge_rating >= 1)? m.challenge_rating : "1/" + (1/m.challenge_rating)}
+                                    </Box>
+                                    <Box title="XP">
+                                        {m.challenge_rating * 200}
+                                    </Box>
+                                </div>
+                            </div>
+                            <div>
+                                <Box title="Languages">
+                                    {m.languages}
+                                </Box>
+                            </div>
+                            <div>
+                                <Box title="Traits">
+                                    <Skills monster={m}/>
+                                    {m.damage_vulnerabilities && <div><b>Damage Vulnerabilities</b>{m.damage_vulnerabilities}</div>}
+                                    {m.damage_resistances && <div><b>Damage Resistances</b>{m.damage_resistances}</div>}
+                                    {m.damage_immunities && <div><b>Damage Immunities</b>{m.damage_immunities}</div>}
+                                    <AbilitySaves monster={m}/>
+                                </Box>
+                            </div>
                         </div>
                     )}
                 </div>
                 <div className="row no-print">
                     <div className="col">
-                        {this.state.monsters.map((m)=>
+                        {monsters.map((m)=>
                             <div key={m.index} className="row">
                                 <div className="col">{m.name}</div>
                                 <div className="col">{m.type}</div>
@@ -114,7 +215,6 @@ class App extends React.Component {
 
     onFilterChange(e){
         this.setState({filter:e.target.value});
-        this.updateSearch(e.target.value, this)
     }
 
     challengeRating(c){
